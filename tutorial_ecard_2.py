ed7ff4714ec109d6c664c475fee56875e3f800a6
xjkdev <xjk2008@gmail.com>
2018-03-31 20:22:09 +0800

    fix pager issue

--- tutorial_ecard_2.py
+++ tutorial_ecard_2.py
@@ -7,6 +7,7 @@ import random
 import matplotlib.pyplot as plt
 from PIL import Image
 import pandas as pd
+import re
 
 
 url_login = 'http://ecard.neu.edu.cn/SelfSearch/Login.aspx'
@@ -98,12 +99,18 @@ def get_all_table(session, consume_soup, startDate, endDate):
 
     list_page = consume_soup.find(
         id='ContentPlaceHolder1_AspNetPager1').find_all('a')
-    if len(list_page) > 3:
-        a = int(list_page[-3].text)
-        for i in range(2, a + 1):
-            consume_soup, tmp = get_table(
-                session, consume_soup, startDate, endDate, page=i)
-            info_table.append(tmp)
+    maxpager = 1
+    for a in list_page:
+        if 'href' not in a.attrs:
+            continue
+        matched = re.match(".*\'(?P<num>\d+)\'", a['href'])
+        if matched is not None:
+            pager = int(matched.group('num'))
+            maxpager = max(pager, maxpager)
+    for i in range(2, maxpager + 1):
+        consume_soup, tmp = get_table(
+            session, consume_soup, startDate, endDate, page=i)
+        info_table.append(tmp)
     return info_table
 
 
@@ -112,7 +119,9 @@ def saveto_csv(info_table, name):
     list2 = []
     for line in info_table.find_all('tr'):
         for thh in line.find_all('th'):
-            # .text 是beautifulsoup的内部方法 存在但并没有在文档中说明 可以抓取该标签(对象)的内容 与.string不同的是 .text可以把当前标签对象内的子标签的内容也抓下来
+            # .text 是beautifulsoup的内部方法 存在但并没有在文档中说明
+            # 可以抓取该标签(对象)的内容 与.string不同的是
+            # .text可以把当前标签对象内的子标签的内容也抓下来
             list1.append(thh.text)
         for tdd in line.find_all('td'):
             list2.append(tdd.text)
@@ -124,7 +133,9 @@ def saveto_csv(info_table, name):
     for head, content in zip(cycle(list1), list2):
         data.setdefault(head, []).append(content)
     frame = pd.DataFrame(data)
-    # 如果不要列头 即中文部分 可以增加参数 header=False 而后想要把多页表格输出到统一表格时  需要加入参数 model = 'a' (default为'w')
+    # 如果不要列头 即中文部分 可以增加参数
+    # header=False 而后想要把多页表格输出到统一表格时
+    # 需要加入参数 model = 'a' (default为'w')
     frame.to_csv(name, index=False, sep=',')
 
 
